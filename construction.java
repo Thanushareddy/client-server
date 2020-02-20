be474335cde9f364f5a76f43e36f538a32f537d7
Thanushareddy <48981064+Thanushareddy@users.noreply.github.com>
2020-02-20 22:41:30 +0530

    Add files via upload

---
+++ construction.java
@@ -0,0 +1,6 @@
+package client_program;
+
+public interface construction { 
+		String Constructions_Materials(String Material_Standard,int Area ); 
+
+	}
---
+++ presented.java
@@ -0,0 +1,7 @@
+package presentation_layer;
+
+public interface presented {
+int Compound_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+	
+	int Simple_Interest_Action(int Principle_Amount, int Rate_Of_Interest, int Time );
+}
